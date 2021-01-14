d79991c28f89a87301588a512ae30399ff5e3b4e
ritsuxis <ritsuxis@gmail.com>
2021-01-14 20:35:47 +0900

    add googleAuth

--- src/SignInScreen.js
+++ src/SignInScreen.js
@@ -11,6 +11,7 @@ const uiConfig = {
   signInSuccessUrl: '/',
   // We will display Google and Facebook as auth providers.
   signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
   ],
 };
